993509b06d1f610f26c681ab312605e2864b440b
Sam Potts <sam@potts.es>
2018-10-02 23:42:04 +1000

    Default width fix

--- selz-ecommerce/includes/fields/width.php
+++ selz-ecommerce/includes/fields/width.php
@@ -1,5 +1,3 @@
-<?php $min = 'widget' == $instance['type'] ? '240' : '160'; ?>
-
 <div class="control-group embed-width" data-type="button,widget">
     <div class="control-label">
         <p class="faux-label"><?php _e( 'Width', $this->lang ); ?></p>
@@ -19,9 +17,9 @@
     <div class="controls">
         <div class="control control-range">
             <label for="<?php echo $this->get_field_id( 'width' ); ?>" class="sr-only"><?php _e( 'Width', $this->lang ); ?></label>
-            <input type="range" max="1000" min="<?php esc_html_e( $min ); ?>" step="5" value="<?php esc_html_e( $min ); ?>" id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>">
+            <input type="range" max="1000" min="160" step="5" value="<?php echo( $instance['width'] ); ?>" id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>">
             <span class="control-range-value">
-                <output for="<?php echo $this->get_field_id( 'width' ); ?>"><?php esc_html_e( $min ); ?></output>px
+                <output for="<?php echo $this->get_field_id( 'width' ); ?>"><?php echo( $instance['width'] ); ?></output>px
             </span>
         </div>
     </div>
