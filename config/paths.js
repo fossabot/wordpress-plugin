5dcd8deeae12f13c218ef43b58dce155b0bb2f14
Michael Worobec <eekumu1@hotmail.com>
2019-01-09 11:34:30 +1100

    remove unused paths

--- config/paths.js
+++ config/paths.js
@@ -13,9 +13,6 @@ const resolvePlugin = relativePath => path.resolve( pluginDir, relativePath );
 
 // Config after eject: we're in ./config/
 module.exports = {
-	dotenv: resolvePlugin( '.env' ),
-	pluginSrc: resolvePlugin( 'src' ), // Plugin src folder path.
 	pluginBlocksJs: resolvePlugin( 'src/js/blocks.js' ),
-	yarnLockFile: resolvePlugin( 'yarn.lock' ),
 	pluginDist: resolvePlugin( '.' ), // We are in ./dist folder already so the path '.' resolves to ./dist/.
 };
