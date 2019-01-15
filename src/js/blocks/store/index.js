7241ca94e0e2f93fc9866a444c95f2c05687be5f
Michael Worobec <eekumu1@hotmail.com>
2019-01-15 14:12:33 +1100

    allow for multiple store blocks per page

--- src/js/blocks/store/index.js
+++ src/js/blocks/store/index.js
@@ -15,9 +15,6 @@ export default {
         // prettier-ignore
         icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><G fill="none" fill-rule="evenodd" transform="translate(2 2)"><G transform="translate(11 11)"><Rect width="8" height="8" x=".5" y=".5" stroke="#DBE3E8" rx="1" /><Path fill="#DBE3E8" fill-opacity=".5" fill-rule="nonzero" d="M1 0h7a1 1 0 0 1 1 1v4H0V1a1 1 0 0 1 1-1z" /><Rect width="2" height="1" x="2" y="6" fill="#DBE3E8" fill-rule="nonzero" rx=".5" /><Rect width="2" height="1" x="5" y="6" fill="#DBE3E8" fill-rule="nonzero" rx=".5" /></G><G transform="translate(0 11)"><Rect width="8" height="8" x=".5" y=".5" stroke="#DBE3E8" rx="1" /><Path fill="#DBE3E8" fill-opacity=".5" fill-rule="nonzero" d="M1 0h7a1 1 0 0 1 1 1v4H0V1a1 1 0 0 1 1-1z" /><Rect width="2" height="1" x="2" y="6" fill="#DBE3E8" fill-rule="nonzero" rx=".5" /><Rect width="2" height="1" x="5" y="6" fill="#DBE3E8" fill-rule="nonzero" rx=".5" /></G><G transform="translate(11)"><Rect width="8" height="8" x=".5" y=".5" stroke="#DBE3E8" rx="1" /><Path fill="#DBE3E8" fill-opacity=".5" fill-rule="nonzero" d="M1 0h7a1 1 0 0 1 1 1v4H0V1a1 1 0 0 1 1-1z" /><Rect width="2" height="1" x="2" y="6" fill="#DBE3E8" fill-rule="nonzero" rx=".5" /><Rect width="2" height="1" x="5" y="6" fill="#DBE3E8" fill-rule="nonzero" rx=".5" /></G><Rect width="8" height="8" x=".5" y=".5" stroke="#DBE3E8" rx="1" /><Path fill="#DBE3E8" fill-opacity=".5" fill-rule="nonzero" d="M1 0h7a1 1 0 0 1 1 1v4H0V1a1 1 0 0 1 1-1z" /><Rect width="2" height="1" x="2" y="6" fill="#DBE3E8" fill-rule="nonzero" rx=".5" /><Rect width="2" height="1" x="5" y="6" fill="#DBE3E8" fill-rule="nonzero" rx=".5" /></G></SVG>,
         attributes,
-        supports: {
-            multiple: false,
-        },
         edit,
         save: props => <Embed {...props} />,
     },
