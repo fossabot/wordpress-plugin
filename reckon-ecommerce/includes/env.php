039f4a2a6e33e56a27aa2c1193a561735fb5fb4e
Michael Worobec <eekumu1@hotmail.com>
2020-02-05 17:58:40 +1100

    update selz references

--- reckon-ecommerce/includes/env.php
+++ reckon-ecommerce/includes/env.php
@@ -5,11 +5,11 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
-<?php if (selz()->developer) { ?>
+<?php if (reckon()->developer) { ?>
     <div style="float: left;">
-        <label for="<?php echo selz()->slug . "_env" ?>" style="float: left;">
-            <span class="sr-only"><?php _e('Environment', selz()->lang); ?></span>
-            <input type="text" id="<?php echo selz()->slug; ?>_env" name="<?php echo selz()->slug; ?>_settings[env]" class="input-control input-control--small" placeholder="selz.com" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" value="<?php echo $options['env']; ?>">
+        <label for="<?php echo reckon()->slug . "_env" ?>" style="float: left;">
+            <span class="sr-only"><?php _e('Environment', reckon()->lang); ?></span>
+            <input type="text" id="<?php echo reckon()->slug; ?>_env" name="<?php echo reckon()->slug; ?>_settings[env]" class="input-control input-control--small" placeholder="selz.com" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" value="<?php echo $options['env']; ?>">
         </label>
         <button type="submit" class="btn btn-small btn-secondary">Save</button>
     </div>
